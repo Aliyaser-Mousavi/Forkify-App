555ea56f8cfe221858c6e542f81dd693fc1e3290
Aliyaser <aliyasermousavi@gmail.com>
2025-12-09 14:57:12 +0430

    add a little time

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = "https://forkify-api.jonas.io/api/v2/recipes/";
-export const TIMEOUT_SEC = 20;
+export const TIMEOUT_SEC = 60;
 export const RES_PER_PAGE = 10;
 export const KEY = "4746da49-acb2-4b75-8fb8-42adcecc9baf";
-export const MODAL_CLOSE_SEC = 2.5;
+export const MODAL_CLOSE_SEC = 5.5;
